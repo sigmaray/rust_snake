9595eb9f5b251d0ff9a093a6dc4c3bd4ee199f2d
Stanislav Zaleski <sigmaray@gmail.com>
2025-01-28 18:05:16 +0300

    1) Improve game logic 2) Improve readme

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ This is a simple snake game implemented in Rust using the `pancurses` library fo
 
 ## Features
 
-- Move the snake using arrow keys
-- Automatic movement with a configurable timeout
+* Move the snake using arrow keys
+* Automatic movement with a configurable timeout
+* Random food generation
+* You win when snake fills all game board cells
+* After you win, you can restart the game by pressing 'r' key
+* The snake can intersect with itself (this was implemented intentionally)
+* The snake can go beyond the playing field (this was implemented intentionally)
 
 ## Requirements
 
@@ -38,3 +43,22 @@ docker run -it "$(docker build -q .)"
 
 - Use the arrow keys to change the direction of the snake.
 - The snake will move automatically if `USE_TIMER` is set to true.
+
+# Environment Variables
+
+You can configure the game using the following environment variables:
+
+* TIMER (enabled by default) - setting it to "0", "false", or "off" will disable the timer; other values will enable the timer
+* SIZE (default: 5) - size of the game board
+* TIMEOUT (default: 300) - timeout for the timer in milliseconds
+Examples:
+```
+TIMER=off SIZE=10 cargo run
+```
+```
+TIMEOUT=500 cargo run
+```
+
+# Demo
+
+![til](https://raw.githubusercontent.com/sigmaray/rust_snake/master/rust_snake.png)
--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,54 @@
+mod state;
+
 extern crate pancurses;
 
 use pancurses::{curs_set, endwin, initscr, noecho, Input};
+use std::env;
 
-// Size of game board in both directions (x, y)
-const BOARD_SIZE: usize = 10;
-
-// Move the snake every TIMEOUT_IN_MS milliseconds or manually with arrow keys
-const USE_TIMER: bool = true;
-
-// Timeout in milliseconds for moving the snake
-const TIMEOUT_IN_MS: i32 = 300;
-
-#[derive(Clone, Debug)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
 #[derive(Debug)]
-struct State {
-    x: usize,
-    y: usize,
-    direction: Direction,
+pub struct EnvConfig {
+    pub use_timer: bool,
+    pub size: usize,
+    pub timeout: i32,
 }
 
-impl State {
-    fn change_direction(&mut self, new_direction: Direction) {
-        self.direction = new_direction;
-    }
+// Parse environment variables passed to a program
+// TIMER (enabled by default) - setting it to "0" or "false" or "off" will disable timer, other values will enable timer
+// SIZE (5 by default) - size of the game board
+// TIMEOUT (300 by default) - timeout for timer
+pub fn parse_env() -> EnvConfig {
+    let use_timer = match env::var("TIMER")
+        .unwrap_or_default()
+        .to_lowercase()
+        .as_str()
+    {
+        "0" | "false" | "off" => false,
+        _ => true,
+    };
 
-    fn move_forward(&mut self) {
-        match self.direction {
-            Direction::Up => {
-                if self.y > 0 {
-                    self.y -= 1;
-                } else {
-                    self.y = BOARD_SIZE - 1
-                }
-            }
-            Direction::Down => {
-                if self.y < BOARD_SIZE - 1 {
-                    self.y += 1;
-                } else {
-                    self.y = 0
-                }
-            }
-            Direction::Left => {
-                if self.x > 0 {
-                    self.x -= 1;
-                } else {
-                    self.x = BOARD_SIZE - 1
-                }
-            }
-            Direction::Right => {
-                if self.x < BOARD_SIZE - 1 {
-                    self.x += 1;
-                } else {
-                    self.x = 0
-                }
-            }
-        }
-    }
+    let size = env::var("SIZE")
+        .ok()
+        .and_then(|s| s.parse::<usize>().ok())
+        .filter(|&s| s > 1)
+        .unwrap_or(5);
 
-    // Generate an empty board (2d vector filled with "_")
-    fn gen_empty_board_vec(&self) -> Vec<Vec<&'static str>> {
-        let mut board = vec![vec![""; BOARD_SIZE]; BOARD_SIZE];
-        for row in board.iter_mut() {
-            for cell in row.iter_mut() {
-                *cell = "_";
-            }
-        }
-        board
-    }
+    let timeout = env::var("TIMEOUT")
+        .ok()
+        .and_then(|t| t.parse::<i32>().ok())
+        .filter(|&t| t > 1)
+        .unwrap_or(300);
 
-    // Convert state into the board (2d array with characters)
-    pub fn to_board_vec(&self) -> Vec<Vec<&str>> {
-        // Generate an empty board
-        let mut board = self.gen_empty_board_vec();
-
-        // Place the food on the board
-        board[self.y][self.x] = "*";
-        board
-    }
-
-    // Convert state into string that renders the board
-    pub fn to_board_str(&self) -> String {
-        let board = self.to_board_vec();
-        let mut board_str = String::new();
-
-        for row in board.iter() {
-            for cell in row.iter() {
-                board_str.push_str(cell);
-            }
-            board_str.push_str("\n");
-        }
-
-        board_str
+    EnvConfig {
+        use_timer,
+        size,
+        timeout,
     }
 }
 
 fn main() {
-    let mut state = State {
-        x: 0,
-        y: 0,
-        direction: Direction::Right,
-    };
+    let config = parse_env();
+
+    let mut state = state::State::new(false, config.size);
 
     let window = initscr();
 
@@ -118,63 +61,69 @@ fn main() {
     // Hide cursor
     curs_set(0);
 
-    window.printw("Type things. Press 'q' to quit\n");
+    if config.use_timer {
+        window.timeout(config.timeout);
+    }
+
+    window.mvprintw(
+        0,
+        0,
+        "Move snake with arrow keys. Press 'q' to quit. Press 'r' to restart\n",
+    );
 
     // Render game board
-    window.mvprintw(2, 0, state.to_board_str());
-
-    window.timeout(TIMEOUT_IN_MS);
+    window.mvprintw(4, 0, state.to_board_str());
 
     loop {
         let input = window.getch();
         let output = &format!("Input: {:?}           ", input);
-        window.mvprintw(1, 0, output);
+        window.mvprintw(2, 0, output);
 
         match input {
             Some(Input::KeyUp) => {
-                state.change_direction(Direction::Up);
-                if !USE_TIMER {
-                    state.move_forward()
-                }
+                state.on_key_press(state::AllowedKeys::Up);
             }
             Some(Input::KeyDown) => {
-                state.change_direction(Direction::Down);
-                if !USE_TIMER {
-                    state.move_forward()
-                }
+                state.on_key_press(state::AllowedKeys::Down);
             }
             Some(Input::KeyLeft) => {
-                state.change_direction(Direction::Left);
-                if !USE_TIMER {
-                    state.move_forward()
-                }
+                state.on_key_press(state::AllowedKeys::Left);
             }
             Some(Input::KeyRight) => {
-                state.change_direction(Direction::Right);
-                if !USE_TIMER {
-                    state.move_forward()
-                }
+                state.on_key_press(state::AllowedKeys::Right);
             }
             Some(Input::Character(c)) => {
                 if c.to_string().to_lowercase() == "q" {
                     // Exit when "q" is pressed
                     break;
+                } else if c.to_string().to_lowercase() == "r" {
+                    state.on_key_press(state::AllowedKeys::Reset);
+                }
+            }
+            None => {
+                if config.use_timer {
+                    state.move_snake()
                 }
             }
             _ => {
-                if USE_TIMER {
-                    state.move_forward()
-                }
+                // Some other key was pressed that we don't care about
             }
         }
 
         // Render game board
-        window.mvprintw(2, 0, state.to_board_str());
+        window.mvprintw(4, 0, state.to_board_str());
+
+        let game_status = if state.did_win {
+            "You won         "
+        } else {
+            "Game in progress"
+        };
+        window.mvprintw((config.size + 6).try_into().unwrap(), 0, game_status);
 
         window.mvprintw(
-            (BOARD_SIZE + 3).try_into().unwrap(),
+            (config.size + 8).try_into().unwrap(),
             0,
-            &format!("state: {:?}     ", state),
+            &format!("Debug output: {:?}     ", state),
         );
     }
     endwin();
